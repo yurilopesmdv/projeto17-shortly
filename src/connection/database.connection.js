891f74439f3cb68ef51850a8a4f1a54e74539fb8
yurilopesm <yurilopesm@hotmail.com>
2023-05-21 18:19:57 -0300

    updating database.connection

--- src/connection/database.connection.js
+++ src/connection/database.connection.js
@@ -7,6 +7,6 @@ const { Pool } = pg
 const configDatabase = {
     connectionString: process.env.DATABASE_URL,
 }
-configDatabase.ssl = true
+if(process.env.MODE === "prod") configDatabase.ssl = true
 
 export const db = new Pool(configDatabase)
